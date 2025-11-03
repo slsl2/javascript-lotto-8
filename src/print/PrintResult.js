76b8cdbc235236f113072d3bcc77225f6bcc0ec0
slsl2 <kimminji7153@gmail.com>
2025-11-03 12:50:34 +0900

    feat(print): 당첨 내역과 수익률 출력하는 함수 생성

---
+++ src/print/PrintResult.js
@@ -0,0 +1,14 @@
+import { Console } from '@woowacourse/mission-utils';
+
+export function printPurchasedLottos(result, totalRate) {
+  Console.print('당첨 통계');
+  Console.print('---');
+
+  Console.print(`3개 일치 (5,000원) - ${result[3]}개`);
+  Console.print(`4개 일치 (50,000원) - ${result[4]}개`);
+  Console.print(`5개 일치 (1,500,000원) - ${result[5]}개`);
+  Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${result['5b']}개`);
+  Console.print(`6개 일치 (2,000,000,000원) - ${result[6]}개`);
+
+  Console.print(`총 수익률은 ${totalRate}%입니다.`);
+}
