5b9fbbd2ebf9fbd4bb7cba1295c69f239332aa1f
slsl2 <kimminji7153@gmail.com>
2025-11-03 12:34:34 +0900

    feat(input): 보너스 번호 중복 예외 처리 추가

--- src/input/InputBonusNumber.js
+++ src/input/InputBonusNumber.js
@@ -1,6 +1,14 @@
 import { Console } from '@woowacourse/mission-utils';
+import { validatelottoNumber } from '../utils/ValidateLottoNumber';
+import { fail } from '../utils/Fail';
 
-export async function inputBonusNumber() {
+export async function inputBonusNumber(winningNumbers) {
   const bonusNumberInput = await Console.readLineAsync();
-  return bonusNumberInput;
+  const bonusNumber = validatelottoNumber(bonusNumberInput);
+
+  if (winningNumbers.includes(bonusNumber)) {
+    fail('보너스 번호는 당첨 번호와 중복될 수 없습니다.');
+  }
+
+  return bonusNumber;
 }
