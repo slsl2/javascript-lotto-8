3bc0702ac6113f775127e35b8c005e34a5a4673a
slsl2 <kimminji7153@gmail.com>
2025-11-03 12:15:13 +0900

    fix(utils): ValidateAmount.js 파일 import 오타 수정

--- src/utils/ValidateAmount.js
+++ src/utils/ValidateAmount.js
@@ -1,8 +1,8 @@
 import { fail } from './Fail.js';
-import { validateIntegerNumber } from './validateIntegerNumber.js';
+import { validateIntNumber } from './ValidateIntNumber.js';
 
 export function validateAmount(amountInput) {
-  const amount = validateIntegerNumber(amountInput, '로또 구입 금액');
+  const amount = validateIntNumber(amountInput, '로또 구입 금액');
 
   if (amount % 1000 !== 0) {
     fail('로또 구입 금액은 1,000원 단위로만 입력 가능합니다.');
